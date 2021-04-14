4ee5d1b5436be2686d6c6655279fab439607e709
Ole Walberg <greblavelo@gmail.com>
2021-04-14 17:12:31 +0200

    made some cool button code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const mainColor = 'darkcyan';
+const secondaryColor = 'palevioletred';
+
 const Button = styled.button`
     cursor: pointer;
     background: transparent;
     font-size: 16px;
     border-radius: 3px;
-    color: palevioletred;
+    color: ${props => (props.primary ? mainColor : secondaryColor)};
+    border: ${props => (props.primary ? `2px solid ${mainColor}` : `2px solid ${secondaryColor}`)}
     border: 2px solid palevioletred;
     margin: 0 1em;
     padding: 0.25em 1em;
     transition: 0.5s all ease-out;
 
     &:hover {
-        background-color: palevioletred;
+        background-color: ${props => 
+            props.primary ? mainColor : secondaryColor};
         color: white;
     }
 `;
@@ -23,7 +28,8 @@ export const App = () => {
         <div>
             <h1>Hello CodeSandbox</h1>
             <h2>Start editing to see some magic happend!</h2>
-            <Button>I am a Button</Button>
+            <Button primary>I am a Button</Button>
+            <Button>I am another Button</Button>
         </div>
     )
 }

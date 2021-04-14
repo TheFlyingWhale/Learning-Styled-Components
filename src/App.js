d2bd1d3a13b98214e66dfc0d52ea4b52564c1ddf
Ole Walberg <greblavelo@gmail.com>
2021-04-14 19:42:23 +0200

    added some new codes which im not a 100% how works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const mainColor = 'darkcyan';
 const secondaryColor = 'palevioletred';
@@ -23,6 +23,38 @@ const Button = styled.button`
     }
 `;
 
+const GreenButton = styled(Button)`
+    background-color: white;
+    color: green;
+    border-color: green;
+`;
+
+const completeMixin = css`
+    color: ${props => (props.whiteColor ? 'white' : 'black')};
+`;
+
+const StyledComp = styled.div`
+    ${props => (props.complex ? completeMixin : 'color:blue')};
+`;
+
+const NButton = styled.button`
+    cursor: pointer;
+    background: transparent;
+    font-size: 16px;
+    border-radius: 3px;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+    transition: 0.5s all ease-out;
+
+    ${props => props.primary &&
+        css`
+        background-color: white;
+          color: green;
+          border-color: green;
+      `};
+`;
+
 export const App = () => {
     return (
         <div>
@@ -30,6 +62,12 @@ export const App = () => {
             <h2>Start editing to see some magic happend!</h2>
             <Button primary>I am a Button</Button>
             <Button>I am another Button</Button>
+            <GreenButton primary>I am a Primary Button</GreenButton>
+            <NButton primary>I am a primary NButton</NButton>
+            <NButton>I am a NButton</NButton>
+            <StyledComp complex whiteColor={true}>I should be white</StyledComp>
+            <StyledComp complex> I should be black</StyledComp>
+            <StyledComp> I should be blue</StyledComp>
         </div>
     )
 }
